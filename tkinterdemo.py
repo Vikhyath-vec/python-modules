270cbd2521d18b6d9ce9b31cb26b65e82f46d8cd
Vikhyath-vec <vikhyath.kothamasu@gmail.com>
2021-05-01 15:29:57 +0530

    made changes

--- tkinterdemo.py
+++ tkinterdemo.py
@@ -6,13 +6,13 @@ import tkinter
 # tkinter._test()
 mainWindow = tkinter.Tk()
 mainWindow.title("Hello World")
-mainWindow.geometry('640x480+8+400')
+mainWindow.geometry('640x480-8-200')
 # widthxheight+x_coord+y_coord
 label = tkinter.Label(mainWindow, text="Hello World")
-label.pack(side="top")
+label.grid(row=0, column=0)
 
 leftFrame = tkinter.Frame(mainWindow)
-leftFrame.pack(side="left", anchor='n', fill=tkinter.Y, expand=False)
+leftFrame.grid(row=1, column=1)
 
 canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
 # canvas.pack(side='left', fill=tkinter.Y) -----> for y
@@ -20,17 +20,17 @@ canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
 # canvas.pack(side='top', fill=tkinter.X) -----> for x
 # canvas.pack(side='left', fill=tkinter.X, expand=True) -----> for x
 # canvas.pack(side='top', fill=tkinter.BOTH, expand=True)
-canvas.pack(side='left', anchor="n")
+canvas.grid(row=1, column=0)
 
 rightFrame = tkinter.Frame(mainWindow)
-rightFrame.pack(side="right", anchor = "n", expand=True)
+rightFrame.grid(row=1, column=2)
 
 button1 = tkinter.Button(rightFrame, text="Button1")
 button2 = tkinter.Button(rightFrame, text="Button2")
 button3 = tkinter.Button(rightFrame, text="Button3")
-button1.pack(side="top")
-button2.pack(side="top")
-button3.pack(side="top")
+button1.grid(row=0, column=0)
+button2.grid(row=1, column=0)
+button3.grid(row=2, column=0)
 
 mainWindow.mainloop()
 
